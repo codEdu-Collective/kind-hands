67c7bd62ca1bfcb44eb7cac243a914ecf67d6665
Abdullah Koyuncu <wisewebworks@outlook.com>
2025-11-03 09:17:56 +0000

    Initialize Next.js 16 and create .env.example file (#5)
    * feat: initialize Next.js 16 project with Tailwind
    
    * add .env.example file

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { JSX } from "react";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kind Hands",
+  description: "Connects donors and volunteers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>): JSX.Element {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { JSX } from "react";
+
+export default function Home(): JSX.Element {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="text-6xl">Kind Hands</main>
+    </div>
+  );
+}
