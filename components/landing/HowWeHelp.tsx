7dc2d058c30e0bad10fbc6845e0954d58fda06a9
Abdullah Koyuncu <wisewebworks@outlook.com>
2025-11-14 21:13:14 +0100

    Landing page components updated
    -OurCouses
    -HowWeHelp
    -WhatWeDo
    -WhatWeDid

--- components/landing/HowWeHelp.tsx
+++ components/landing/HowWeHelp.tsx
@@ -1,7 +1,50 @@
 import { JSX } from "react";
+import Image from "next/image";
 
 const HowWeHelp = (): JSX.Element => {
-  return <section className="w-full h-247 bg-foreground text-white">HowWeHelp</section>;
+  return (
+    <section className="w-full h-247 bg-foreground text-white overflow-hidden relative">
+      <Image
+        src={`/images/aboutUs-kids.jpg`}
+        width={1920}
+        height={988}
+        alt="kids"
+        className="opacity-10 blur-xl object-center"
+      />
+      <div className="absolute top-0 left-0 grid grid-cols-2 py-35.5 px-90 gap-14">
+        <aside>
+          <h3 className="font-sue-ellen text-4xl mb-3">How We Help</h3>
+          <h2 className="font-shippori font-extrabold text-[2.5rem] mb-3">
+            Join The Community To Give Education For Children
+          </h2>
+          <p className="text-justify font-work text-xl mb-4">
+            Lorem Ipsum is simply dummy text of the printin typesetting dummy text ever when an
+            unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
+          <div className="w-full h-48 relative mb-8">
+            <Image src={`/images/howHelp-kids.jpg`} alt="kids" fill className="object-cover" />
+          </div>
+          <button className="uppercase w-50 h-16 bg-primary rounded-4xl text-white text-lg font-work font-medium">
+            Donate NOW
+          </button>
+        </aside>
+        <aside className="border border-white px-16 py-12">
+          <h2 className="font-shippori font-extrabold text-4xl">Become A Volunteer</h2>
+          {["Full Name*", "Email Address*", "Phone Number*"].map((item, index) => (
+            <div key={index} className="w-full h-16 border border-white my-8">
+              <p className="p-4 font-work text-lg">{item}</p>
+            </div>
+          ))}
+          <div className="w-full h-39 border border-white my-8">
+            <p className="p-5 font-work text-lg">Message*</p>
+          </div>
+          <button className="uppercase w-50 h-16 bg-background rounded-4xl text-black text-lg font-work font-medium">
+            Donate NOW
+          </button>
+        </aside>
+      </div>
+    </section>
+  );
 };
 
 export default HowWeHelp;
--- components/landing/OurCouses.tsx
+++ components/landing/OurCouses.tsx
@@ -1,7 +1,73 @@
 import { JSX } from "react";
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
+import Image from "next/image";
 
 const OurCouses = (): JSX.Element => {
-  return <section className="w-full h-332 bg-beige">OurCouses</section>;
+  return (
+    <section className="w-full h-332 bg-beige flex flex-col items-center justify-center">
+      <div className="w-300">
+        <h3 className="font-sue-ellen text-4xl text-primary">Our Couses</h3>
+        <div className="flex items-center justify-between ">
+          <h2 className="font-shippori font-extrabold text-[2.5rem]">
+            You can help lots of people by <br />
+            donating little.
+          </h2>
+          <button className="uppercase w-50 h-16 bg-primary rounded-4xl text-white text-lg font-work font-medium">
+            More Causes
+          </button>
+        </div>
+      </div>
+      <div className="flex px-9 gap-9 mt-12">
+        {[
+          { percent: 85, raised: 5, goal: 10 },
+          { percent: 75, raised: 3, goal: 10 },
+          { percent: 50, raised: 5, goal: 10 },
+          { percent: 75, raised: 5, goal: 10 },
+        ].map(({ percent, raised, goal }, index) => (
+          <div key={index} className="relative">
+            <Image
+              src={`/images/causes-${index + 1}.jpg`}
+              width={437}
+              height={387}
+              alt="kids"
+              className="object-cover"
+            />
+            <div className="absolute top-6 left-6 bg-background">
+              <p className="font-shippori font-extrabold text-2xl text-primary px-3 py-2">
+                {percent}%
+              </p>
+            </div>
+            <div className="bg-background w-full p-8">
+              <h2 className="font-shippori text-2xl font-extrabold mb-8">
+                Big charity: build school for poor children
+              </h2>
+              <div className="w-full h-1.5 bg-[#C5CECC] rounded-3xl" />
+              <div className="w-2/3 h-1.5 bg-primary relative -top-1.5 rounded-3xl z-2" />
+              <div className="flex justify-between mt-5 mb-9 font-shippori font-extrabold text-xl">
+                <p>Raised - {raised}M</p>
+                <p className="text-primary">Goal - ${goal}M</p>
+              </div>
+              <button className="uppercase w-36 h-16 bg-dark rounded-4xl text-white text-lg font-work font-medium">
+                Donate
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex mt-12 gap-75">
+        <MdOutlineKeyboardArrowLeft
+          size={73}
+          color="black"
+          className="bg-background rounded-full overflow-hidden"
+        />
+        <MdOutlineKeyboardArrowRight
+          size={73}
+          color="black"
+          className="bg-background rounded-full overflow-hidden"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default OurCouses;
