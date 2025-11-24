3f50b047869821aa809ae450723faead7d9193e7
Bahar <ylmzbhr1@gmail.com>
2025-11-24 22:29:47 +0300

    Refactor About Page and AboutMore Component for Improved Readability
    - Clean up JSX structure by removing unnecessary line breaks and optimizing layout for clarity.
    - Enhance text formatting for better readability and consistency.
    - Update AboutMore component content for improved flow and organization.

--- components/about/AboutMore.tsx
+++ components/about/AboutMore.tsx
@@ -9,19 +9,16 @@ const AboutMore = () => {
       {showMore && (
         <>
           <p className="text-base md:text-xl text-justify text-neutral mb-6">
-            It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
+            It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
           <p className="text-base md:text-xl text-justify text-neutral mb-6">
-            The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-            below for those interested. Sections 1.10.32 and 1.10.33 from de
-            Finibus Bonorum et Malorum by Cicero are also reproduced in their
-            exact original form, accompanied by English versions from the 1914
-            translation by H. Rackham.
+            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those
+            interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero
+            are also reproduced in their exact original form, accompanied by English versions from
+            the 1914 translation by H. Rackham.
           </p>
         </>
       )}
