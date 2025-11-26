a75e50a22cd436856ef3f75b201f8a5394bf2ae5
Abdullah Koyuncu <wisewebworks@outlook.com>
2025-11-26 18:08:01 +0100

    linting

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,10 +1,5 @@
+import { FaFacebook, FaInstagram, FaPinterest, FaSquareXTwitter } from "react-icons/fa6";
 import Image from "next/image";
-import {
-  FaFacebook,
-  FaInstagram,
-  FaPinterest,
-  FaSquareXTwitter,
-} from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -18,12 +13,8 @@ const Home = () => {
           className="object-cover"
         />
         <div className="relative z-2 text-white ml-90">
-          <h1 className="font-sue-ellen text-5xl mb-1.5">
-            Humanity Peoples...
-          </h1>
-          <h2 className="font-shippori font-extrabold text-[5.625rem]">
-            Team Member
-          </h2>
+          <h1 className="font-sue-ellen text-5xl mb-1.5">Humanity Peoples...</h1>
+          <h2 className="font-shippori font-extrabold text-[5.625rem]">Team Member</h2>
         </div>
       </header>
       <section className="flex justify-center">
@@ -44,9 +35,7 @@ const Home = () => {
                 height={382}
                 className="object-cover"
               />
-              <h4 className="my-4.5 font-shippori font-extrabold text-xl">
-                {member}
-              </h4>
+              <h4 className="my-4.5 font-shippori font-extrabold text-xl">{member}</h4>
               <div className="flex justify-center gap-4">
                 <FaInstagram size={18} aria-label="Instagram icon" />
                 <FaFacebook size={18} aria-label="Facebook icon" />
@@ -58,20 +47,15 @@ const Home = () => {
         </div>
       </section>
       <section className="h-137 relative flex justify-center items-center">
-        <Image
-          src={`/teams/Volunteer.jpg`}
-          alt="hero image"
-          fill
-          className="object-cover"
-        />
+        <Image src={`/teams/Volunteer.jpg`} alt="hero image" fill className="object-cover" />
         <div className="relative z-2 text-white text-center w-193">
           <h1 className="font-sue-ellen text-4xl">Become Volunteer</h1>
           <h2 className="font-shippori font-extrabold text-[2.5rem] mt-0.5 mb-4">
             Join Our Organization
           </h2>
           <h3 className="font-work text-xl">
-            Save the Children believes every child deserves a future. In the
-            U.S. and around the world, we give children a healthy start in life.
+            Save the Children believes every child deserves a future. In the U.S. and around the
+            world, we give children a healthy start in life.
           </h3>
           <button className="font-work text-lg bg-dark w-49 h-16 rounded-full mt-10">
             Join Us Now
