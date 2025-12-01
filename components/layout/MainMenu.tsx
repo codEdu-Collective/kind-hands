cd20f4545368f80df2681445069f3498cf896840
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-12-01 23:13:12 +0300

    feat(menu): add volunteer  link

--- components/layout/MainMenu.tsx
+++ components/layout/MainMenu.tsx
@@ -63,6 +63,12 @@ export default function MainMenu() {
                     >
                       Team
                     </Link>
+                    <Link
+                      href="/volunteer"
+                      className="block px-4 py-2 hover:bg-gray-50 dark:hover:bg-gray-700 text-dark dark:text-gray-200 transition-colors"
+                    >
+                      Volunteer
+                    </Link>
                   </div>
                 </div>
               </div>
