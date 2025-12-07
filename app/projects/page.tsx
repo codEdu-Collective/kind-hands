f9ac246b51c559f8a2be721c260e2aca39dbdc2f
Bahar <ylmzbhr1@gmail.com>
2025-12-07 13:37:00 +0300

    feat(projects): add project page and assets
    - Created a new Project component with a layout featuring a header and a grid of project images.
    - Added project images (1-9) and a banner image for the project page.
    - Implemented responsive design for better viewing on different screen sizes.

---
+++ app/projects/page.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+const Project = () => {
+  return (
+    <main className="w-full">
+      <header className="relative h-96 md:h-205.5 flex items-center">
+        <Image
+          src="/projects/project-banner.png"
+          alt="hero"
+          fill
+          className="object-cover"
+        />
+        <div className="relative z-2 text-white ml-4 md:ml-90 max-w-full md:max-w-120 px-4 md:px-0">
+          <h1 className="font-sue-ellen text-2xl md:text-5xl mb-1.5">
+            Donation
+          </h1>
+          <h2 className="font-shippori font-extrabold text-3xl md:text-[5.625rem] leading-tight">
+            Meet Charity Projects
+          </h2>
+        </div>
+      </header>
+      <section className="container mx-auto items-center w-full max-w-200 px-4 md:px-0 my-8 md:my-37.5">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
+          {[...Array(9)].map((_, index) => {
+            if (index === 5) {
+              return (
+                <div
+                  key={index}
+                  className="relative bg-primary flex flex-col items-center justify-center w-[200px] h-[230px]"
+                >
+                  <p className="text-white text-center text-lg md:text-xl font-semibold px-3 leading-tight">
+                    More than One Life Changed
+                  </p>
+                </div>
+              );
+            }
+
+            return (
+              <div key={index} className="relative w-[200px] h-[230px]">
+                <Image
+                  src={`/projects/project-${index + 1}.png`}
+                  alt="project"
+                  width={200}
+                  height={230}
+                  className="object-cover"
+                />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Project;
