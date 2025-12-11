1b5100f869135a77f45ca798f12cb2618489b7d0
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-12-11 21:59:15 +0300

    feat(nav): add Blog link to MainMenu for easy navigation

--- components/layout/MainMenu.tsx
+++ components/layout/MainMenu.tsx
@@ -69,6 +69,12 @@ export default function MainMenu() {
                     >
                       Volunteer
                     </Link>
+                    <Link
+                      href="/blog"
+                      className="block px-4 py-2 hover:bg-gray-50 dark:hover:bg-gray-700 text-dark dark:text-gray-200 transition-colors"
+                    >
+                      Blog
+                    </Link>
                   </div>
                 </div>
               </div>
