d3989ea586603038429e552e03ba575a00508f18
Bahar <ylmzbhr1@gmail.com>
2025-12-14 12:58:23 +0300

    style(projects): enhance layout and spacing for Project component
    - Reformatted JSX for improved readability and consistency.
    - Adjusted dimensions of project cards and images for better visual alignment.
    - Updated section max-width and grid gap for a more cohesive design.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,22 +3,30 @@ const Project = () => {
   return (
     <main className="w-full">
       <header className="relative h-96 md:h-205.5 flex items-center">
-        <Image src="/projects/project-banner.png" alt="hero" fill className="object-cover" />
+        <Image
+          src="/projects/project-banner.png"
+          alt="hero"
+          fill
+          className="object-cover"
+        />
         <div className="relative z-2 text-white ml-4 md:ml-90 max-w-full md:max-w-180 px-4 md:px-0">
-          <h1 className="font-sue-ellen text-2xl md:text-5xl mb-1.5">Donation</h1>
+          <h1 className="font-sue-ellen text-2xl md:text-5xl mb-1.5">
+            Donation
+          </h1>
           <h2 className="font-shippori font-extrabold text-3xl md:text-[5.625rem] leading-tight ">
             Meet Charity Projects
           </h2>
         </div>
       </header>
-      <section className="container mx-auto items-center w-full max-w-200 px-4 md:px-0 my-8 md:my-37.5">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-items-center">
+      <section className="container max-w-250 mx-auto items-center px-4 md:px-0 my-8 md:my-37.5">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-5 justify-items-center">
+          
           {[...Array(9)].map((_, index) => {
             if (index === 5) {
               return (
                 <div
                   key={index}
-                  className="relative bg-primary flex flex-col items-center justify-center w-[200px] h-[230px] min-h-[230px]"
+                  className="relative bg-primary flex flex-col items-center justify-center w-[240px] h-[275px] min-h-[275px]"
                 >
                   <p className="text-white text-center text-lg md:text-xl font-semibold px-3 leading-tight">
                     More than One Life Changed
@@ -28,12 +36,15 @@ const Project = () => {
             }
 
             return (
-              <div key={index} className="relative w-[200px] h-[230px] min-h-[230px]">
+              <div
+                key={index}
+                className="relative w-[240px] h-[275px] min-h-[275px]"
+              >
                 <Image
                   src={`/projects/project-${index + 1}.png`}
                   alt="project"
-                  width={200}
-                  height={230}
+                  width={240}
+                  height={275}
                   className="object-cover w-full h-full"
                 />
               </div>
