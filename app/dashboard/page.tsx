919c0fb2664d1d7b68a45b8bb6e6abb3b345aaf5
Abdullah Koyuncu <wisewebworks@outlook.com>
2025-12-21 18:35:12 +0100

    add Suspense for /dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,19 @@
+import { Suspense } from "react";
+
 import { verifySession } from "@/lib/session";
 
 export default async function DashboardPage() {
-  const { userId } = await verifySession();
   return (
     <div>
       <h1>Dashboard</h1>
-      <h2>Welcome, User {userId}!</h2>
+      <Suspense fallback={<p>Loading user info...</p>}>
+        <UserInfo />
+      </Suspense>
     </div>
   );
 }
+
+const UserInfo = async () => {
+  const { userId } = await verifySession();
+  return <h2>Welcome, User {userId}!</h2>;
+};
